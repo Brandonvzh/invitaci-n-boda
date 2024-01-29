dc07045356dbc4a942c4a6ed49b257ef0f3b3b8d
Brandon Vazquez <bhernandez17@lenovo.com>
2024-01-28 20:40:20 -0600

    v0.2.6:

--- core/settings.py
+++ core/settings.py
@@ -86,11 +86,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
+
+import dj_database_url
+# Heroku DB configurations
+db_from_env = dj_database_url.config(conn_max_age = 500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
